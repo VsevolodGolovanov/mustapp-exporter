49266ccd33e26672410973771cda0b1d013e2917
Seva Golovanov <VsevolodGolovanov@users.noreply.github.com>
2025-04-16 23:02:50 +0200

    Fetching user lists; added lodash and yup

--- src/routes/user/[username]/+page.svelte
+++ src/routes/user/[username]/+page.svelte
@@ -8,6 +8,5 @@
 		<li>Want: {data.lists.want.length}
 		<li>Watched: {data.lists.watched.length}
 		<li>Series: {data.lists.shows.length}
-		<li>YouTube: {data.lists.youtube.length}
 	</ul>
 </div>
--- src/routes/user/[username]/MustAppService.ts
+++ src/routes/user/[username]/MustAppService.ts
@@ -1,4 +1,5 @@
 import { error } from '@sveltejs/kit';
+import { array, type InferType, number, object, string } from 'yup';
 
 export class MustAppService {
 	public static async getProfile(username: string) {
@@ -9,9 +10,6 @@ export class MustAppService {
 	private static async fetchUserPageSrc(username: string) {
 		const response = await fetch(`https://mustapp.com/@${encodeURIComponent(username)}/want`, {
 			method: 'GET',
-			headers: {
-				'content-type': 'text/html'
-			},
 			cache: 'reload'
 		});
 
@@ -19,7 +17,7 @@ export class MustAppService {
 			error(
 				response.status,
 				`${response.status === 404 ? 'Invalid username?' : ''}\
-			Failed to fetch ${response.url}: ${response.statusText}`
+					Failed to fetch ${response.url}: ${response.statusText}`
 			);
 		}
 
@@ -37,17 +35,87 @@ export class MustAppService {
 
 		const profileValueJsonStr = userPageSrc.substring(profileValueStartIdx, profileValueEndIdx);
 
-		return JSON.parse(profileValueJsonStr) as Profile;
+		return profileSchema.cast(profileValueJsonStr, { stripUnknown: true });
+	}
+
+	public static async getUserProductList(
+		userId: number,
+		productIds: number[]
+	): Promise<UserProductList> {
+		const response = await fetch(
+			`https://mustapp.com/api/users/id/${userId}/products?embed=product,review`,
+			{
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json'
+				},
+				body: JSON.stringify({ ids: productIds }),
+				cache: 'reload'
+			}
+		);
+
+		if (!response.ok) {
+			error(response.status, `Failed to fetch ${response.url}: ${response.statusText}`);
+		}
+
+		const responseText = await response.text();
+		return userProductListSchema.cast(responseText, { stripUnknown: true });
 	}
 }
 
-// only the stuff we need
-export type Profile = {
-	lists: {
-		want: number[];
-		shows: number[];
-		watched: number[];
-		youtube: number[];
-	};
-	id: number;
-};
+const idListSchema = array(number().required()).required();
+
+const profileSchema = object({
+	id: number().required(),
+	lists: object({
+		want: idListSchema,
+		shows: idListSchema,
+		watched: idListSchema
+		// youtube: idListSchema
+	}).required()
+	// skip other stuff
+})
+	.required()
+	.json();
+
+export type Profile = InferType<typeof profileSchema>;
+
+const userProductListSchema = array(
+	object({
+		userProductInfo: object({
+			// "modified_at": "2020-01-01T00:00:00.000000Z",
+			// "user_id": <id>,
+			// "product_id": <id>,
+			// "status": "watched",
+			// "discussion_id": <id>,
+			rate: number().nullable()
+			// "reviewed": true,
+			// "review": {
+			// 	"reviewed_at": "2020-01-01T00:00:00.000000Z",
+			// 	"body": "review"
+			// },
+			// "is_disliked": false,
+			// "is_notification_enabled": null
+		})
+			.required()
+			.camelCase(),
+		product: object({
+			// "id": <id>,
+			// "type": "movie",
+			title: string().required()
+			// "release_date": "2015-01-01",
+			// "poster_file_path": "/<...>.jpg",
+			// "trailer_url": "https://www.youtube.com/watch?v=<...>",
+			// "items_count": 0,
+			// "items_released_count": 0,
+			// "subtitle": null,
+			// "runtime": 1000
+		})
+			.required()
+			.camelCase()
+	}).camelCase()
+)
+	.required()
+	.json();
+
+export type UserProductList = InferType<typeof userProductListSchema>;
